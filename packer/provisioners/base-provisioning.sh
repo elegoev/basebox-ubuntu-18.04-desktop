1ed4aeaaabf8fe6ddd1cdcf137adf4249f624017
Urs Voegele <unext@bluewin.ch>
2021-02-08 16:34:29 +0100

    new basebox version 1.0.1612797669

--- packer/provisioners/base-provisioning.sh
+++ packer/provisioners/base-provisioning.sh
@@ -11,6 +11,38 @@ sudo apt-get -y install `check-language-support -l de`
 # set locale
 sudo update-locale LANG=de_CH.UTF-8
 
+# # install xrdp
+sudo apt install xrdp -y
+sudo apt install xorg-video-abi-24 -y
+sudo apt install xserver-xorg-input-all -y
+sudo apt install xorgxrdp -y
+
+# # grant access
+sudo adduser xrdp ssl-cert
+
+# # restart xrdp
+sudo systemctl stop xrdp
+sudo systemctl start xrdp
+
+# set password for ubuntu user
+echo -e "ubuntu\nubuntu" | sudo passwd ubuntu
+
+# set .xsession
+echo gnome-session > /home/vagrant/.xsession
+sudo chmod +x /home/vagrant/.xsession
+sudo chown vagrant:vagrant /home/vagrant/.xsession
+
+sudo cp /home/vagrant/.xsession /home/ubuntu/.xsession
+sudo chmod +x /home/ubuntu/.xsession
+sudo chown ubuntu:ubuntu /home/ubuntu/.xsession
+
+# copy keymap
+sudo cp /home/vagrant/files-prov/xrdp/km-00000807.ini /etc/xrdp/km-00000807.ini
+sudo chmod 644 /etc/xrdp/km-00000807.ini
+
+# copy polkit rule
+sudo cp /home/vagrant/files-prov/xrdp/45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
+
 # set version
 UBUNTU_VERSION=$(lsb_release -a | grep Release | awk  '{print $2}')
 echo "# Installed application "  > $application_file_path
